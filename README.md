5f35eb6568348a39cd74ac1fc74be7a9262040ac
Ethan Ransom <derethanhausen@gmail.com>
2023-07-03 13:39:42 -0600

    make it fast w/ mmap & SIMD

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # `mergediff`
 
-Mergediff is a CLI utility to compare two presorted CSV files and output the differences. It pairs well with the output of `sort`, if the files are not presorted. It compares to the tool `diff` but does not load the files into memory, allowing for comparison of very large files.
+Mergediff is a CLI utility to compare two presorted CSV files and output the differences. It pairs well with the output of `sort`, if the files are not presorted. It compares to the tool `diff` but does not load the files into memory, allowing for comparison of very large files. Incidentally it is also generally faster, see [performance](#performance) for more information.
 
 ## Usage
 
@@ -9,3 +9,17 @@ Mergediff will append creates (lines in file1 but not in file2), deletes (lines 
 ```bash
 mergediff <file1> <file2> <output dir>
 ```
+
+## Performance
+
+Mergediff is benchmarked primarily against `diff`.
+
+Why mergediff is fast, non-exhaustive, in approximate order of importance:
+
+* **Assuming presorted input**: Mergediff can avoid loading the entire file into memory, saving the initial load step and keeping the memory footprint low.
+* **Avoiding allocations**: While the rust standard library provides an excellent [`.lines()`](https://doc.rust-lang.org/std/io/trait.BufRead.html#method.lines) iterator, it allocates a new `String` for each line, which is massively slow.
+  * Interestingly, Rust has since shipped Generic Associated Types, which I believe would allow for a `.lines()` implementation that returns a `&str` instead of a `String`. While it seems unlikely that the stdlib would break the API of `BufRead` to add this, it could be a good opportunity for a third-party crate.
+  * Sidebar: this tool was written to replace a Ruby equivalent; even the idiomatic, overallocating stdlib rust implementation was 6x faster.)
+* **Use of `bstr`**: Mergediff uses the [bstr](https://crates.io/crates/bstr) crate extracted from the (paradigm-changingly-good) [`ripgrep`](https://github.com/BurntSushi/ripgrep) tool to search for newlines in the input.
+  * We can skip utf8 validation, which isn't as expensive as you'd first expect, but it still unnecessary overhead.
+  * `bstr` provides a fast implementation of `find_byte`, which is used to find newlines and primary keys.
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,62 @@
 use std::cmp::Ordering;
 use std::fs::File;
 use std::fs::OpenOptions;
-use std::io::BufRead;
-use std::io::BufReader;
 use std::io::BufWriter;
 use std::io::Result;
 use std::io::Write;
+
+use bstr::ByteSlice;
+use fastcmp::Compare;
 
 fn mergediff(new: File, old: File, creates: File, updates: File, deletes: File) -> Result<()> {
     let mut creates = BufWriter::new(creates);
     let mut updates = BufWriter::new(updates);
     let mut deletes = BufWriter::new(deletes);
 
-    let mut new = BufReader::new(new).lines().peekable();
-    let mut old = BufReader::new(old).lines().peekable();
+    // SAFETY: mmapping a file gives you no protection from the file changing out from under you.
+    //         Theoretically this possibility is always present with files, however not having a
+    //         buffer amplifies the chance.
+    let new_file = unsafe { memmap::Mmap::map(&new)? };
+    let old_file = unsafe { memmap::Mmap::map(&old)? };
 
-    while let (Some(new_line), Some(old_line)) = (new.peek(), old.peek()) {
-        let new_line = new_line.as_ref().expect("error reading line from newfile");
-        let old_line = old_line.as_ref().expect("error reading line from oldfile");
+    let mut new_iter = new_file.as_bstr().lines_with_terminator().peekable();
+    let mut old_iter = old_file.as_bstr().lines_with_terminator().peekable();
 
-        if new_line == old_line {
-            new.next();
-            old.next();
+    while let (Some(new), Some(old)) = (new_iter.peek(), old_iter.peek()) {
+        if new.feq(old) {
+            new_iter.next();
+            old_iter.next();
             continue;
         }
 
-        let (new_pk, _) = new_line.split_once(',').unwrap_or((new_line, ""));
-        let (old_pk, _) = old_line.split_once(',').unwrap_or((old_line, ""));
+        let new_pk = &new[0..new.as_bstr().find_byte(b',').unwrap_or(0)];
+        let old_pk = &old[0..old.as_bstr().find_byte(b',').unwrap_or(0)];
 
         match new_pk.cmp(old_pk) {
             Ordering::Equal => {
-                writeln!(updates, "{}", new_line)?;
-                new.next();
-                old.next();
+                updates.write_all(new)?;
+                new_iter.next();
+                old_iter.next();
             }
             Ordering::Less => {
-                writeln!(creates, "{}", new_line)?;
-                new.next();
+                creates.write_all(new)?;
+
+                new_iter.next();
             }
             Ordering::Greater => {
-                writeln!(deletes, "{}", old_line)?;
-                old.next();
+                deletes.write_all(old)?;
+
+                old_iter.next();
             }
         }
     }
 
-    for line in old {
-        writeln!(deletes, "{}", line.unwrap())?;
+    for line in old_iter {
+        deletes.write_all(line)?;
     }
 
-    for line in new {
-        writeln!(creates, "{}", line.unwrap())?;
+    for line in new_iter {
+        creates.write_all(line)?;
     }
 
     // Flushing the buffers b/c buffers will automatically flush when they go out-of-scope,
@@ -77,7 +83,7 @@ fn test_mergediff() {
     .unwrap();
 
     fn contents(path: &str) -> String {
-        std::fs::read_to_string(path).expect("couldn't read file")
+        std::fs::read_to_string(path).expect(&format!("couldn't read file: {path}"))
     }
 
     assert_eq!(
@@ -94,48 +100,17 @@ fn test_mergediff() {
     );
 }
 
-#[test]
-fn test_single_column_merge_diff() {
-    std::fs::create_dir_all("tmp/").expect("couldn't mkdir tmp/");
-
-    mergediff(
-        File::open("fixtures/single-column/target.csv").unwrap(),
-        File::open("fixtures/single-column/original.csv").unwrap(),
-        File::create("tmp/creates_2.csv").expect("couldn't open tmp/creates.csv for writing"),
-        File::create("tmp/updates_2.csv").expect("couldn't open tmp/updates.csv for writing"),
-        File::create("tmp/deletes_2.csv").expect("couldn't open tmp/deletes.csv for writing"),
-    )
-    .expect("it should succeed.");
-
-    fn contents(path: &str) -> String {
-        std::fs::read_to_string(path).expect("couldn't read file")
-    }
-
-    assert_eq!(
-        contents("tmp/creates_2.csv"),
-        contents("fixtures/single-column/creates.csv")
-    );
-    assert_eq!(
-        contents("tmp/updates_2.csv"),
-        contents("fixtures/single-column/updates.csv")
-    );
-    assert_eq!(
-        contents("tmp/deletes_2.csv"),
-        contents("fixtures/single-column/deletes.csv")
-    );
-}
-
 fn main() {
-    let args: [String; 3] = std::env::args()
+    let paths: [String; 3] = std::env::args()
         .skip(1) // binary name always 0th arg
         .collect::<Vec<String>>()
         .try_into()
         .expect("requires three arguments as input:\n<newfile> <oldfile> <output dir>");
 
-    let new_file = File::open(&args[0]).expect("could not open first file");
-    let old_file = File::open(&args[1]).expect("could not open second file");
+    let new_file = File::open(&paths[0]).expect("could not open first file");
+    let old_file = File::open(&paths[1]).expect("could not open second file");
 
-    let output_path = std::path::Path::new(&args[2]);
+    let output_path = std::path::Path::new(&paths[2]);
 
     let [creates, updates, deletes] = ["creates.csv", "updates.csv", "deletes.csv"].map(|file| {
         OpenOptions::new()
